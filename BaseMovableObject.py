af8f7e3a04567a34805ad3344cd0968681b22c3b
MadHolm <arturkepka41@gmail.com>
2018-11-13 08:14:34 +0100

    takie jakby omijanie

--- BaseMovableObject.py
+++ BaseMovableObject.py
@@ -1,4 +1,5 @@
 from vectorCalculator import *
+import math
 
 
 class BaseMovableObject(pg.sprite.Sprite):
@@ -12,6 +13,9 @@ class BaseMovableObject(pg.sprite.Sprite):
         self.vel = pg.math.Vector2(0, 0)
         self.speed = speed
         self.angle = 0
+        self.vector = 0
+        self.perception_point_one = pg.math.Vector2(0, 0)
+        self.perception_point_two = pg.math.Vector2(0, 0)
 
     def update(self):
         self.image = pg.transform.rotozoom(self.orig_img, -self.angle, 1)
@@ -22,6 +26,8 @@ class BaseMovableObject(pg.sprite.Sprite):
     def rotate(self, face_target):
         _, self.angle = (face_target-self.pos).as_polar()
 
+    #def vector(self, vector):
+
     def do_move(self, target_position):
         target_vector = sub(target_position, self.pos)
         move_vector = [c * self.speed for c in normalize(target_vector)]
@@ -29,5 +35,89 @@ class BaseMovableObject(pg.sprite.Sprite):
 
     def stop(self):
         self.vel = pg.math.Vector2(0, 0)
+
+    def search_range(self, range, distacne):
+        # Perception range and distance
+        self.perception_range = range
+        self.perception_distance = distacne
+
+        # Perception points - testing these for potential collision
+        self.perception_point_one_x = self.perception_distance * math.cos(
+            math.radians(self.angle + self.perception_range))
+        self.perception_point_one_y = self.perception_distance * math.sin(
+            math.radians(self.angle + self.perception_range))
+
+        self.perception_point_one = pg.math.Vector2(self.pos[0] + self.perception_point_one_x,
+                                                    self.pos[1] + self.perception_point_one_y)
+
+        self.perception_point_two_x = self.perception_distance * math.cos(
+            math.radians(self.angle - self.perception_range))
+        self.perception_point_two_y = self.perception_distance * math.sin(
+            math.radians(self.angle - self.perception_range))
+
+        self.perception_point_two = pg.math.Vector2(self.pos[0] + self.perception_point_two_x,
+                                                    self.pos[1] + self.perception_point_two_y)
+
+    def do_move_avoid_obstacle(self, target_position, obstacle_position, is_collision, direction):
+
+        target_vector = sub(target_position, self.pos)
+
+        if is_collision == True:
+            #Force vector pushing the object away from the obstacle
+            obstacle_vector = sub(obstacle_position, self.pos)
+
+            force_vector_one = sub( self.perception_point_one, obstacle_position )#add( obstacle_vector, self.perception_point_one )
+            force_vector_two = sub( self.perception_point_two, obstacle_position ) #sub( obstacle_vector, self.perception_point_two)
+
+            if direction == 0:
+                move_vector = [c * self.speed for c in normalize( force_vector_one )]
+
+            elif direction == 1:
+                move_vector = [c * self.speed for c in normalize( force_vector_two )]
+
+
+            self.vel = move_vector
+
+
+        elif is_collision == False:
+
+
+            move_vector = [c * self.speed for c in normalize(target_vector)]
+            self.vel = move_vector
+            print("NOT AVOIDING")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #print("avoiding")
+        # elif math.hypot(obstacle_position[0] - self.perception_point_two[0],
+        #               obstacle_position[1] - self.perception_point_two[1]) < obstacle_radius:
+        #     move_vector = [c * self.speed for c in normalize(add(target_vector, self.force_vector_two))]
+        #     self.angle = 1.01 * self.angle
+        #     print("avoiding")
+        #
+        # self.vel = move_vector
+        # target_vector = sub(target_position, self.pos)
+        # obstacle_vector = sub(obstacle_position, self.pos)
+        # move_vector = [c * self.speed for c in normalize(add( target_vector, obstacle_vector))]
+        # if direction == 1:
+        #     self.angle = 0.5 * self.angle
+        #     self.vel = move_vector
+        # else:
+        #     self.angle = 1.1 * self.angle
+        #     self.vel = move_vector
 
 
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import random
-
+import array
 from BaseMovableObject import *
 from vectorCalculator import *
 
@@ -16,7 +16,7 @@ PINK = (255, 200, 200)
 class MapElement(pg.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        radius = random.randint(5, 50)
+        radius = random.randint(50, 50)
 
         self.image = pg.Surface((2*radius, 2*radius), pg.SRCALPHA)
         pg.draw.circle(
@@ -34,6 +34,8 @@ def PlaceObjects( obj_number , all_objects ):
         y_obj = random.randint(1, 480)
         map_obj_i = MapElement((x_obj, y_obj))
         all_objects.add(map_obj_i)
+    # map_obj_i = MapElement((320, 240))
+    # all_objects.add(map_obj_i)
 
 
 class Player(BaseMovableObject):
@@ -67,18 +69,70 @@ class Player(BaseMovableObject):
         self.vel = pg.math.Vector2(0, 0)
 
 
+class Enemy(BaseMovableObject):
+    def __init__(self, vec_pos, speed):
+        self.image = pg.Surface((50, 40), pg.SRCALPHA)
+        pg.draw.polygon(
+            self.image,
+            pg.Color('red'),
+            ((1, 1), (49, 20), (1, 39)))
+        BaseMovableObject.__init__(self, self.image, vec_pos, speed)
+
+    def rotate_enemy(self, player_pos):
+        self.rotate(player_pos)
+
+    # def enemy_do_move(self, player_pos):
+    #     self.do_move(player_pos)
+
+    def enemy_do_move(self, target_position, obstacle_position, is_collision, direction):
+        self.do_move_avoid_obstacle(target_position, obstacle_position, is_collision, direction)
+
+
+
+    def stop(self):
+        self.vel = pg.math.Vector2(0, 0)
+
+
+    # def avoid_obstacle(self, enemy_pos, enemy_angle, obstacle_position, obstacle_radius):
+    #     self.epos = enemy_pos
+    #     self.eang = enemy_angle
+    #     self.opos = obstacle_position
+    #     self.orad = obstacle_radius
+    #
+    #     # 1,20    100,1 oraz 100,40 - te punkty sprawdzaj dla kolizji
+    #     if (enemy_pos.x
+
+
 def main():
     screen = pg.display.set_mode((640, 480))
     clock = pg.time.Clock()
     all_sprites = pg.sprite.Group()
-    player = Player((320, 240), 2)
-    all_sprites.add(player)
+    player = Player((640, 0), 3)
+    enemy = Enemy((0, 480), 2)
+    all_sprites.add(player, enemy)
 
     all_objects = pg.sprite.Group()
-    PlaceObjects(20, all_objects)
+    object_array = []
+    direction = 0
+
+    for i in range(5):
+        x_obj = random.randint(1, 640)
+        y_obj = random.randint(1, 480)
+        map_obj = MapElement((x_obj, y_obj))
+        object_array.append(map_obj.pos)
+        all_objects.add(map_obj)
+
+    #map_obj_1 = MapElement((200,200))
+    #all_objects.add(map_obj_1)
+    #PlaceObjects(5, all_objects)
+
+    print(len(object_array))
+    print(object_array[i][0])
+    print(enemy.perception_point_one[0])
 
     done = False
     while not done:
+        is_collision = False
         player.rotate_player()
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
@@ -87,6 +141,34 @@ def main():
             player.do_dodge(0)
         elif keys[pg.K_UP]:
             player.player_do_move()
+
+
+        enemy.search_range(15, 40)
+        for i in range (len(object_array)):
+            if math.sqrt((object_array[i][0] - enemy.perception_point_one[0]) * (
+                    object_array[i][0] - enemy.perception_point_one[0]) + (
+                                 object_array[i][1] - enemy.perception_point_one[1]) * (
+                                 object_array[i][1] - enemy.perception_point_one[1])) < 50:
+                is_collision = True
+                direction = 0
+                break
+            elif math.sqrt((object_array[i][0] - enemy.perception_point_two[0]) * (
+                    object_array[i][0] - enemy.perception_point_two[0]) + (
+                                 object_array[i][1] - enemy.perception_point_two[1]) * (
+                                 object_array[i][1] - enemy.perception_point_two[1])) < 50:
+                is_collision = True
+                direction = 1
+                break
+
+        enemy.enemy_do_move(player.pos, (object_array[i][0], object_array[i][1]), is_collision, direction)
+        enemy.rotate_enemy(player.pos)
+
+            # elif math.hypot(obstacle_position[0] - self.perception_point_two[0],
+            #               obstacle_position[1] - self.perception_point_two[1]) < obstacle_radius:
+            #     move_vector = [c * self.speed for c in normalize(add(target_vector, self.force_vector_two))]
+            #     self.angle = 1.01 * self.angle
+            #     print("avoiding")
+
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
